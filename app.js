7b42335611be5ca5bb049b901803822a21187057
Orkhand4 <orkhand4@mail.ru>
2024-09-15 11:59:28 +0400

    onClick, arrow func

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const btn_tag = document.querySelector('#btn')
 
-function demo() {
-  alert('salam')
-}
+// function demo() {
+//   alert('salam')
+// }
 
-btn_tag.addEventListener('click', demo)
+// btn_tag.addEventListener('click', demo)
+
+btn_tag.addEventListener('click', ()=>{
+  alert('salam')
+})
